793c99721bb2f14dcd6f7ff28caf82329dea3836
Roberto Fierros Zepeda <robfz729@gmail.com>
2018-08-19 21:12:38 -0500

    Expose actual Users through the / endpoint

--- src/rest-api.js
+++ src/rest-api.js
@@ -6,8 +6,19 @@ const PORT = process.env.PORT || 8000;
 const db = getDatabaseConnection('sequelize_test', 'roberto', '');
 const api = express();
 
-api.get('/', (req, res) => {
-  res.json({ success: true });
+async function handler(req, res) {
+  const User = db.import(`${__dirname}/db/models/user`);
+
+  const users = await User.findAll();
+
+  res.json({ users });
+}
+
+api.get('/', handler);
+
+api.use((req, res, next) => {
+  req.db = db;
+  next();
 });
 
 async function init() {
